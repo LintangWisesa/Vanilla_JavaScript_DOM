bab9876f691de896779466aefc91f013ee20c960
lintangwisesa <lintangwisesa@ymail.com>
2019-09-15 13:48:07 +0700

    vanilla js

--- 4.style.js
+++ 4.style.js
@@ -1,2 +1,11 @@
 var title = document.getElementById('judul')
+
 title.style.border = 'solid 3px red'
+title.style.color = 'red'
+title.style.backgroundColor = 'yellow'
+title.style.fontFamily = 'Impact'
+
+title.style.bottom = "-100px";
+title.style.right = "120px";
+// title.style.top = "-100px";
+// title.style.left = "120px";
